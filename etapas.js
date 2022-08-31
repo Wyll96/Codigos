2c43b4078f4e8cdf133176fe0613808401e5cb2f
unknown <wylliamdias96@gmail.com>
2022-08-31 14:51:18 -0300

    projetos js

---
+++ etapas.js
@@ -0,0 +1,43 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: '38111',
+            nome: 'LIN FIO FERRUBRABO',
+            partido: 'PPI'
+        }
+        ]
+    },
+    {
+            numero: '77221',
+            nome: 'LIN FIO KANU',
+            partido: 'IUO'
+    },
+
+    {
+
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+
+            {
+
+                numero: '99',
+                nome:'KYU TI PLANTUNABO',
+                partido: 'NUQ',
+                vice: 'CIRAO FALAMERDA'
+            },
+
+            {
+
+                numero: '13 ',
+                nome:'ATAKA FERRUNA RACHADA',
+                partido:'OHC',
+                vice: 'ZUCRANO LI'
+            }
+        ]
+    }
+
+];
